8292e8a7db04f8d2d5800882e1b31032f9e6a4dd
Stephen Smith <sns1215@gmail.com>
2015-01-06 16:37:57 -0500

    Fixed formatting changes suggested by Jack

--- accordion.jsx
+++ accordion.jsx
@@ -1,49 +1,37 @@
     var Accordion = React.createClass({
-      getInitialState: function()
-      {
+      getInitialState: function() {
         return { expanded: null };
       },
-      toggleTabs: function(expandedTabId)
-      {
-        console.log(expandedTabId);
+      toggleTabs: function(expandedTabId) {
         this.setState({expanded:expandedTabId});
       },
-      render: function()
-      {
+      render: function() {
         var self = this;
         var clickHandler = this.toggleTabs;
-        var accordionTabs = this.props.data.map(function (tab, id)
-        {
+        var accordionTabs = this.props.data.map(function (tab, id) {
           return (
             <AccordionTab data={tab} onTabClick={clickHandler} id={id} expanded={self.state.expanded == id}/>
           );
         });
         
         return (
-          <div className="accordion">
-            {accordionTabs}
+          <div className="accordion"> {accordionTabs}
           </div>
         );
       }
     });
 
     var AccordionTab = React.createClass({
-      tabClicked: function ()
-      {
-        console.log(this.props.id);
-
+      tabClicked: function () {
         var newState = null;
-        if(!this.props.expanded)
-        {
+        if(!this.props.expanded) {
           newState = this.props.id
         }
 
         this.props.onTabClick(newState)
       },
-      render: function()
-      {
-        var listItems = this.props.data.children.map(function (tab)
-        {
+      render: function() {
+        var listItems = this.props.data.children.map(function (tab) {
           return (
             <ListItem data={tab}/>
           );
@@ -51,16 +39,14 @@
 
         return (
           <div className="accordionTab">
-            <div className="tabHeader" onClick={this.tabClicked}>{this.props.data.text}</div>
-            { this.props.expanded ? <div ref="list">{listItems}</div> : null }
+            <div className="tabHeader" onClick={this.tabClicked}>{this.props.data.text}</div> { this.props.expanded ? <div ref="list">{listItems}</div> : null }
           </div>
         )
       }
     });
 
     var ListItem = React.createClass({
-      render: function()
-      {
+      render: function() {
         return (
           <div className="listItem">{this.props.data.text}</div>
         )
