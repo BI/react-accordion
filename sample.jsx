7cac13dac54a6affe277b828268d45c78be11586
Stephen Smith <sns1215@gmail.com>
2015-02-24 15:07:20 -0500

    Cleaned up Webpack code to bundle better

--- sample.jsx
+++ sample.jsx
@@ -1,3 +1,4 @@
+var React = require("react");
 var react_accordion = require("./accordion.jsx");
 require("./accordion-styles.css");
 
--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,5 @@ module.exports = {
             { test: /\.jsx$/, loader: "jsx" },
             { test: /\.css$/, loader: "style!css" }
         ]
-    },
-    externals: {
-        //don't bundle the 'react' npm package with our bundle.js
-        //but get it from a global 'React' variable
-        'react': 'React'
     }
 };
